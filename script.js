eccf534599b0af41cfb4949767b4c9af75aababa
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-06 22:49:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //your JS code here. If required.
 function btnclick() {
-	  let counterElement = document.getElementById("counter"); // Get the counter element
-
-        document.getElementById("incrementBtn").addEventListener("click", function() {
-            let currentValue = parseInt(counterElement.textContent); // Get current counter value
-            alert("Current value before increment: " + currentValue); // Show alert with the current value
-            counterElement.textContent = currentValue + 1; // Update the counter text
-        });
+	let counterElement = document.getElementById("counter");
+    let currentValue = parseInt(counterElement.textContent);
+            alert(currentValue);
+            counterElement.textContent = currentValue + 1; 
+        
 		
 	
 	
